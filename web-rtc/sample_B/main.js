1894654340bd95671ffb32ef37b0fd1a325b70c7
Amireza Fatemi <arfo.amireza@gmail.com>
2016-10-23 23:00:29 +0200

    Capturing snap from video stream
    Example how to project video stream into canvas

--- web-rtc/sample_B/main.js
+++ web-rtc/sample_B/main.js
@@ -1,7 +1,21 @@
+var video = document.querySelector('video');
+canvas = document.querySelector('canvas')
+var btn = document.getElementById("capture");
+streaming = false;
+
 function hasUserMedia(){
     return !! (navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.meGetUserMedia)
 }
+
+// Get Available device
+MediaStreamTrack.getSources(function(sources){
+    for (var i = 0; i < sources.lenght; i++){
+        var source = sources[i];
+        console.log(source.kind);
+    }
+});
+
 
 if (hasUserMedia()){
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
@@ -9,8 +23,8 @@ if (hasUserMedia()){
     p = navigator.mediaDevices.getUserMedia({video: true, audio:false});
 
     p.then(function(stream){
-        var video = document.querySelector('video');
         video.src = window.URL.createObjectURL(stream);
+        streaming = true;
     });
 
     p.catch(function(e){
@@ -19,3 +33,13 @@ if (hasUserMedia()){
 } else {
     alert("Sorry, your browser does not support getUserMedia.");
 }
+
+
+btn.addEventListener('click', function(e){
+    if(streaming){
+      canvas.width = video.clientWidth;
+      canvas.height = video.clientHeight;
+      var context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0);
+    }
+})
