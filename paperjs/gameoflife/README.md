95f34c748ee544235ef993fcc9b5f16dc8c0e163
Amireza Fatemi <arfo.amireza@gmail.com>
2016-10-27 00:40:06 +0200

    Very first path
    Also command guid to run the page

---
+++ paperjs/gameoflife/README.md
@@ -0,0 +1,7 @@
+```sh
+$ cd paperjs/gameoflife
+$ npm install
+$ bower install
+$ node server.js
+```
+Then hit the https://localhost:8080
--- paperjs/gameoflife/lib.js
+++ paperjs/gameoflife/lib.js
@@ -1,1 +1,7 @@
   console.log('here we go');
+  // very first path
+  var path = new Path.Circle({
+      center: view.center,
+      radius: 30,
+      strokeColor: 'black'
+  });
