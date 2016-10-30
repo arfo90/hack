6a53eed7c9350b8a39d36c8b8b5666f594e46b50
Amireza Fatemi <arfo.amireza@gmail.com>
2016-10-30 22:17:12 +0100

    In middle of calc alive cell
    stucked and have no clue why!!

--- paperjs/gameoflife/lib.js
+++ paperjs/gameoflife/lib.js
@@ -1,8 +1,8 @@
 var worldGrid = [];
-var worldSize = 60;
+var worldSize = 3;
 
 function seedTheWorld(){
-	for(j = 0; j < worldSize -1; j++){
+	for(j = 0; j < worldSize; j++){
 		console.log('heeers');
 		worldGrid.push(getRow());
 	}
@@ -15,7 +15,7 @@ function getRandomeSeed(){
 
 function getRow(){
 	var row = new Array();
-	for (i = 0; i < worldSize-1;i++){
+	for (i = 0; i < worldSize;i++){
 		row.push(getRandomeSeed());
 	}
 	return row;
@@ -26,10 +26,41 @@ console.log(worldGrid);
 
 function ruleTheWorld(x, y){
 	var aliveNeighbor = 0;
+	var currentNeighborX = setRow(x);
+	var currentNeighborY = setRow(y);
+
 	for (ix = 0; ix < 3; ix++){
-		
+		console.log('this is x ..'+ currentNeighborX + '  this is y ..'+ currentNeighborY);
+		if (currentNeighborX = worldSize){
+			console.log(aliveNeighbor);
+			return;
+		}
+		for (iy = 0; iy < 3; iy++){
+			if (worldGrid[currentNeighborX][currentNeighborY]){
+				aliveNeighbor++;
+			}
+			currentNeighborY++;
+		}
+		currentNeighborX++;
+		currentNeighborY = setRow(y);
 	}
+	if (worldGrid[x][y]){
+		aliveNeighbor--;
+	}
+	console.log(aliveNeighbor);
 }
+
+function setRow(val){
+	if (val == 0){
+		val = 0;
+	} else {
+		val = val - 1;
+	}
+	return val;
+}
+
+console.log(worldGrid[1][1]);
+ruleTheWorld(1,2);
 
 function onFrame(event){
 
