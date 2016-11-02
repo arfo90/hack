7de7ab6b49e54fda01c923b0f28322bd3e1594d5
masoud karimnia <masoud.karimnia@gmail.com>
2016-11-02 19:47:28 +0100

    fixed walls and mouse movement

--- paperjs/gameoflife/lib.js
+++ paperjs/gameoflife/lib.js
@@ -1,12 +1,33 @@
 debugger;
 var screenWidth = myCanvas.width;
 var screenHeight = myCanvas.height;
-var board = new Path.Rectangle(new Point(screenWidth/2, screenHeight-51), new Size(100, 50));
-board.strokeColor = 'black';
-board.strokeWidth = 1;
-board.fill = true;
-board.fillColor = 'black';
+
+var globalStyle = {
+  strokeColor: 'black',
+  strokeWidth: 1,
+  fill: true,
+  fillColor: 'black',
+};
+
+ var board = new Path.Rectangle(new Point(screenWidth/2, screenHeight-51), new Size(100, 50));
+ board.style = globalStyle;
+
+// var leftWall = new Path.Rectangle(new Point(0,0), new Size(50, screenHeight));
+// leftWall.style = globalStyle;
+
+// var rightWall = new Path.Rectangle(new Point(screenWidth-50,0), new Size(50, screenHeight));
+// rightWall.style = globalStyle;
+
+// var topWall = new Path.Rectangle(new Point(0,0), new Size(screenWidth, 50));
+// topWall.style = globalStyle;
+
+var walls = new Path({
+  segments: [[0, screenHeight], [0,0], [screenWidth, 0], [screenWidth, screenHeight]]
+});
+
+walls.strokeColor = 'red';
+walls.strokeWidth = 20;
 
 function onMouseMove(event){
-    board.position.x += event.delta.x;
+    board.position.x = event.point.x;
 }
