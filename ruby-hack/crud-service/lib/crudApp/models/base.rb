88e25ddf9df46fa797f0d1006d58c4dc52891105
Amireza Fatemi <arfo.amireza@gmail.com>
2017-01-13 20:47:58 +0100

    Add active record and mysql2 to model base class
    Model can now get extended from base::Activrecord

---
+++ ruby-hack/crud-service/lib/crudApp/models/base.rb
@@ -0,0 +1,9 @@
+require 'mysql2'
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => 'mysql',
+  :database => 'database',
+  :username => 'user',
+  :password => 'password',
+  :host     => 'localhost')
