27476bb7109ea5febc69df08f73808e3e3c97ca3
Amireza Fatemi <arfo.amireza@gmail.com>
2017-01-15 18:56:00 +0100

    Fix spacing

--- ruby-hack/crud-service/lib/crudApp/models/userModel.rb
+++ ruby-hack/crud-service/lib/crudApp/models/userModel.rb
@@ -1,12 +1,12 @@
 require_relative 'base'
 
-module Crudapp 
+module Crudapp
   module Model
-		class UserModel < ActiveRecord::Base
-	  	self.table_name = "user"
-	  	def puts_s
-				p 'testing'
-	  	end
-		end
+    class UserModel < ActiveRecord::Base
+      self.table_name = "user"
+      def puts_s
+        p 'testing'
+      end
+    end
   end
 end
