f221700ee06348e1cd34b580c7521a86fe96f4cf
Amireza Fatemi <arfo.amireza@gmail.com>
2017-02-23 13:52:54 +0100

    golang array ref

---
+++ go-lang/ref/src/array/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main(){
+    myArray := make([]string, 0)
+    myArray = append(myArray, "num1")
+    myArray = append(myArray, "num2")
+    myArray = append(myArray, "num3")
+    myArray = append(myArray, "num4")
+
+
+    for _, item := range myArray {
+        fmt.Printf(item)
+    }
+}
