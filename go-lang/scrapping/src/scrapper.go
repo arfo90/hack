12e7a4d3ceb74241651e83e13761c894b0ac89c3
Amireza Fatemi <arfo.amireza@gmail.com>
2017-10-29 18:19:07 +0100

    Parse job ads into 3 main string within one map
    later can use the map var to persists date into database

--- go-lang/scrapping/src/scrapper.go
+++ go-lang/scrapping/src/scrapper.go
@@ -5,9 +5,11 @@ import (
   "github.com/PuerkitoBio/goquery"
   "log"
   "strings"
+
+  _"database/sql"
 )
 
-func scrap(url string) []string{
+func scrapUrls(url string) []string{
   //resp, err := goquer.NewDocument("http://berlinstartupjobs.com/engineering/page/1/")
   doc, err := goquery.NewDocument(url)
 
@@ -29,11 +31,32 @@ func scrap(url string) []string{
   return urls
 }
 
+func scrap_job(url string) map[string]string{
+  job := make(map[string]string)
+  doc, err := goquery.NewDocument(url)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Printf("Starting scrapping job ads...")
+
+  job["Title"] = doc.Find("h1.bsj-h1").Text()
+  job["Description"] = doc.Find("div.job-details").Text()
+  job["Details"] = doc.Find("div.w-richtext").Text()
+
+  return job
+}
+
 func main(){
-  urls := scrap("http://berlinstartupjobs.com/engineering/page/1/")
+  urls := scrapUrls("http://berlinstartupjobs.com/engineering/page/1/")
 
   for _, u := range urls {
     url := strings.Replace(u, "location.href=", "", -1)
-    fmt.Printf("%s \n", url)
+    job := scrap_job(strings.Trim(url, "'"))
+    fmt.Printf("%s \n", job["Title"])
+    fmt.Printf("%s \n", job["Description"])
+    fmt.Printf("%s \n", job["Details"])
+    break
   }
 }
